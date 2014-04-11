9069374c12b93d060fcbbb08deee73916855b1f5
Dennis Heimbigner <dmh@ucar.edu>
2014-04-11 13:28:30 -0600

    checkpoint

--- netcdf4/src/main/java/ucar/nc2/jni/netcdf/Nc4Iosp.java
+++ netcdf4/src/main/java/ucar/nc2/jni/netcdf/Nc4Iosp.java
@@ -1827,17 +1827,18 @@ public class Nc4Iosp extends AbstractIOServiceProvider implements IOServiceProvi
     if (ret != 0)
       throw new IOException(ret + ": " + nc4.nc_strerror(ret));
     byte[] entire = bb.array();
-
+    int[] intshape;
     if(shape != null) {
         // fix: this is ignoring the rank of section.
         // was: ArrayObject values = new ArrayObject(ByteBuffer.class, new int[]{len});
-        int[] intshape = new int[shape.length];
+        intshape = new int[shape.length];
         for (int i = 0; i < intshape.length; i++) {
           intshape[i] = shape[i].intValue();
         }
-        ArrayObject values = new ArrayObject(ByteBuffer.class, intshape);
-    } else {//scalar
-    }
+    } else
+        intshape = new int[]{1};
+    ArrayObject values = new ArrayObject(ByteBuffer.class, intshape);
+
     int count = 0;
     IndexIterator ii = values.getIndexIterator();
     while (ii.hasNext()) {
